0634f12acfb8fd6aa959819777907f9bec03a4ae
aepiccolo7 <aepiccolo7@gmail.com>
2013-11-24 17:25:29 -0500

    streaming adc to dac at >44ksps

--- ARMproject/dsping.c
+++ ARMproject/dsping.c
@@ -7,12 +7,12 @@
 
 uint8_t get_adc_sample(void)
 {
-	GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_1, 0x00); // CHANGE TO PORT A CONTORL PINS
-	while(GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_2)|0x04);
-	GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_1, 0x02);
-	uint8_t data = (uint8_t)GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5);
-	data |= 0xC0;
-	data += ((uint8_t)GPIOPinRead(GPIO_PORTC_BASE, GPIO_PIN_6|GPIO_PIN_7))<<6;
+	GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_6, 0x00); // pull RDbar low
+	while(GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_7)); //wait till done
+	GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_6, 0x40); //pull RDbar high
+	uint8_t data = (uint8_t)GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5); //compute value
+	//data |= 0xC0;
+	data |= ((uint8_t)GPIOPinRead(GPIO_PORTC_BASE, GPIO_PIN_6|GPIO_PIN_7));
     return data;
 }
 
--- ARMproject/main.c
+++ ARMproject/main.c
@@ -2,22 +2,31 @@
 #include <stdbool.h>
 #include "inc/hw_types.h"
 #include "inc/hw_memmap.h"
+#include "inc/tm4c123gh6pm.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 #include "dsping.h"
 #include "init.h"
+#include "driverlib/timer.h"
+#include "driverlib/interrupt.h"
+
 
 int main(void)
 {
-	uint8_t value = 0;
-	SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
-	//SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-	//GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+	SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN); //system clock 40 MHz
+	//SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+	//GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_7);
 	init_DAC();
-	//init_ADC();
-	while(1)
-	{
-		give_dac_sample(value);
-		value++;
+	init_ADC();
+
+	uint32_t sample_frequency = 50000;
+	uint32_t sample_period = 30 + (SysCtlClockGet() / sample_frequency); //49.8127 ksps
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+	TimerConfigure(TIMER0_BASE, TIMER_CFG_A_PERIODIC);
+	TimerEnable(TIMER0_BASE, TIMER_A);
+	while(1){
+		TimerLoadSet(TIMER0_BASE, TIMER_A, sample_period);
+	 	give_dac_sample(get_adc_sample());
+		while(TimerValueGet(TIMER0_BASE, TIMER_A) > 50);
 	}
 }
